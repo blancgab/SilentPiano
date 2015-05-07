a2f3bdb46f78928778caabc44b999cf2a72ccf0e
harrisonliew <harrisonliew@gmail.com>
2015-05-07 17:43:23 -0400

    Stabilized w/ No Hand Filter

--- Main.m
+++ Main.m
@@ -1,21 +1,42 @@
 % Silent Piano
 
 vid = VideoReader(fullfile('videos','TwoHanded.mov'));
+addpath('stabilization');
 figure(1);
 
 vidWidth = vid.Width;
 vidHeight = vid.Height;
-hf = figure;
-set(hf,'position',[150 150 vidWidth vidHeight]);
 
-%f = readFrame(vid);
+init_frame = readFrame(vid);
+temp = load('template.mat');
+[~, ~, stable_init] = stabilize_frame(init_frame, temp, 3);
+% createMap(f)
 
-% while hasFrame(vid)
-%     fprev = f;
-%     f = readFrame(vid);
-%     imshow(f);
-%     input('Press any key to continue');
-% end
+n = 1;
 
-diffFrames(vid, 1);
-
+while hasFrame(vid)
+    
+    for i = 1:n
+        if i == n
+            final_frame = readFrame(vid);
+            [~, ~, final_stable] = stabilize_frame(final_frame, temp, 3);
+        else
+            readFrame(vid);
+        end
+    end
+    
+    diff = abs(final_stable-stable_init);
+    
+    subplot(2,1,1);
+    imshow(final_frame);
+    
+    subplot(2,1,2);
+    nhf = noHandsFilter(final_frame);
+    se = ones(4,1);
+    bwd = im2bw(diff,.2);
+    imshow(bwd.*nhf(1:714,1:1274));   
+    
+    stable_init = final_stable;
+    
+    drawnow;
+end
