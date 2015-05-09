6ef43904c74f57546e06265b92e9fedea4fc8abe
Mindy Nguyen <mnn2108@columbia.edu>
2015-05-09 15:21:47 -0400

    Mindy code: peak each frame

--- Mindy.m
+++ Mindy.m
@@ -23,20 +23,20 @@ while hasFrame(vid)
     diff = abs(fs-fprevs);
       
     %se = ones(4,1);
-    %bwd = im2bw(diff,.2);
+    bwd = im2bw(diff,.2);
     d = bwd.*nhf;
     d1 = sum(d);
-    for i = 1:floor(1276/m)
-        d2(1,i) = d1(1,m*i-1)+ d1(1,m*i); %2 col each bin
-        %d2(1,i) = d1(1,m*i-1)+ d1(1,m*i-2)+ d1(1,m*i); %3 col each bin
-    end
+%     for i = 1:floor(1276/m)
+%         d2(1,i) = d1(1,m*i-1)+ d1(1,m*i); %2 col each bin
+%         %d2(1,i) = d1(1,m*i-1)+ d1(1,m*i-2)+ d1(1,m*i); %3 col each bin
+%     end
     subplot(2,1,1);
     imshow(fs); 
     str = sprintf('%f ',count);
     title(str);
     subplot(2,1,2);
     %imshow(d1);
-    plot(d2);
+    plot(d1);
     ylim([0, 200]);
     
     drawnow;
