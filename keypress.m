d013f5ec20c6b9c61fa74594bbbf3a795673ad1f
harrisonliew <harrisonliew@gmail.com>
2015-05-11 21:40:49 -0400

    Update keypress detection algorithm
    changed dilations (map to 5, nohandsfilter to 20)

--- keypress.m
+++ keypress.m
@@ -4,18 +4,37 @@ function keys = keypress(map, diff, stabrad)
 
 n = size(map);
 keycorr = zeros(1, n(1));
-black = ones(1, n(1));
+black = zeros(1, n(1));
 keys = [];
 
 % figure out which are the black keys
 for i = 1:n(1)
-    if isempty(strfind(map{i,1}, 'b'))
-        black(i) = 0;
+    bkey = strfind(map{i,1}, 'b');
+    if bkey{1} == 2
+        black(i) = 1;
     end
 end
 
+% find where the black key ends
+for i = 1:3
+    if black(i) == 1
+        [row1, col1] = find(map{i,2}, 1);
+        break
+    end
+end
+for i = n(1):-1:n(1)-2
+    if black(i) == 1
+        [row2, col2] = find(map{i,2}, 1);
+        break
+    end
+end
+topofblack = min(row1, row2);
+
 % zero pad diff image
 diff = padarray(diff, [stabrad stabrad], 'both');
+% generate diff of white keys only too
+diffwhite = zeros(size(diff));
+diffwhite(1:topofblack-1, :) = diff(1:topofblack-1, :);
 
 % correlate the frames, get correlation sum
 for i = 1:n(1)
@@ -28,17 +47,21 @@ end
 % find local maxima in the correlation vector
 [pks, loc] = findpeaks(keycorr);
 for i = 1:numel(loc)
-    if black(loc(i)) == 1
-        if keycorr(loc+2) ~= 0
+    % adjust for left side of frame
+    if black(loc(i)) == 1 && loc(i) < n(1)/2
+        if keycorr(loc(i)+2) ~= 0
+            loc(i) = loc(i)+1;
+        elseif sum(sum(and(map{loc(i)+1,2}, diffwhite))) ~= 0
             loc(i) = loc(i)+1;
         end
     end
-    if pks(i) > 100 % only get peaks that are clearly keypresses
+    if pks(i) > 150 % only get peaks that are clearly keypresses
         keys = [keys; loc(i)];
     end
 end
+keycorr
 pks
 loc
 
-figure
+% figure
 plot(keycorr)
