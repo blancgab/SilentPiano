37bbb4ac239235f049b2dbc0be476d1a4caf4c80
harrisonliew <harrisonliew@gmail.com>
2015-05-12 02:50:50 -0400

    Improved result!
    changed a few thresholds.
    cleaned up main.

--- Main.m
+++ Main.m
@@ -10,8 +10,6 @@ vid = VideoReader(filename);
 num_frames = vid.NumberOfFrames;
 vid  = VideoReader(filename); % recreate video after getting number of frames
 
-
-
 % get first frame, create map, stabilize
 f = readFrame(vid);
 map = createMap(f, vid.Height);
@@ -41,16 +39,15 @@ count = round(vid.FrameRate * start_time);
 
 map_size = size(map);
 
+binsize = 10; % number of columns per bin
+
 notestoplay = zeros(num_frames, map_size(1));
 last_pressed = count*ones(1, map_size(1)); % for debouncing
 last_released = count*ones(1, map_size(1)); % for debouncing
 DEBOUNCE = 4; %ignore key presses within 4 frames
 RELEASE_TIME = 80;
 
-
-binsize = 10; % number of columns per bin
-
-figure
+% figure
 
 % LOOP
 
@@ -65,21 +62,13 @@ while hasFrame(vid)
     nhf = and(buffer(:,:,1,2), nh(radius+1 : vid.Height-radius, radius+1 : vid.Width-radius));
     
     % change diff to b&w, mask with hand filter
-    diff = new_frame - uint8(buffer(:, :, 1, 1));
-    diff2 = uint8(abs(double(new_frame) - buffer(:, :, 1, 1))); % both positive and negative diffs
+    diff = uint8(abs(double(new_frame) - buffer(:, :, 1, 1))); % both positive and negative diffs
     buffer(:, :, 1, :) = [];
     buffer(:, :, n, 1) = new_frame;
     buffer(:, :, n, 2) = nh(radius+1 : vid.Height-radius, radius+1 : vid.Width-radius);
     
     bwd = im2bw(diff,.3);
-    bwd2 = im2bw(diff2, .3);
-    d = bwd2.*nhf; % final diff
-%     d2 = bwd2.*nhf;
-%     subplot(2, 1, 1);
-%     imshow(d);
-%     subplot(2, 1, 2);
-%     imshow(d2);
-%     drawnow
+    d = bwd.*nhf; % final diff
     
     % determine if key has been pressed
     % make column bins (prioritizes lines)
@@ -91,20 +80,18 @@ while hasFrame(vid)
             d2(1,i) = d2(1,i) + d1(binsize*i-j+1);
         end
     end
-    % if lines are distinct but there isn't too much noise elsewhere, key
-    % has been pressed
     
     % copy last state to new frame
     notestoplay(count, :) = notestoplay(count - 1, :);
     
-    if  max(d2) > 200 && sum(d2)/max(d2) < 3.5
-        subplot(2,1,1)
-        imshow(d);
-        str = sprintf('frame: %i, max: %i, sum: %i', count, max(d2), sum(d2));
-        title(str);
-        subplot(2,1,2)
-        plot(d2)
-        ylim([0 200])
+    % if lines are distinct but there isn't too much noise elsewhere, key
+    % has been pressed
+    if  max(d2) > 250 && sum(d2)/max(d2) < 5
+%         subplot(2,1,1)
+%         imshow(d);
+%         str = sprintf('frame: %i, max: %i, sum: %i', count, max(d2), sum(d2));
+%         title(str);
+%         subplot(2,1,2)
         presses = keypress(map, d, radius)
         for i = 1:size(presses)
             key = presses(i);
@@ -120,17 +107,15 @@ while hasFrame(vid)
         % release any keys that have been on for too long
         
     end
-     to_release = find(and((notestoplay(count, :) > 0), (count - last_pressed > RELEASE_TIME)) > 0);
-     last_released(to_release) = count;
-     notestoplay(count, to_release) = 0;
+    to_release = find(and((notestoplay(count, :) > 0), (count - last_pressed > RELEASE_TIME)) > 0);
+    last_released(to_release) = count;
+    notestoplay(count, to_release) = 0;
      
-    %input('continue ');
     drawnow
     count = count +1;
 end
 
 % parse notestoplay for MIDI function
-
 M = [];
 startframe = 0;
 endframe = 0;
@@ -141,11 +126,8 @@ for i = 1:map_size(1)
         end
         if notestoplay(j, i) == 0 && notestoplay(j-1, i) == 1
             endframe = j-1;
-            M = [M; 1, 1, 73-i, 30, startframe/29.97, endframe/29.97];
+            M = [M; 1, 1, 73-i, 120, startframe/29.97, endframe/29.97];
         end
     end
 end
-writemidi(matrixtomidi(M), 'output.midi');
- 
-
-% play(notestoplay);
+writemidi(matrix2midi(M), 'output.midi');
